a21f08945a09835032dd78f884579110fdecbf36
zhuxiaowei <1649201921@qq.com>
2022-09-06 15:15:43 +0800

    【需求】修改路径

--- src/page/Api.js
+++ src/page/Api.js
@@ -1,7 +1,7 @@
 const Api = {
-  login: "/login",//登陆
-  getVideoList: "/getVideoList", //获取视频列表
-  likeVideo: "/likeVideo",//点赞视频
+  login: "/api/login",//登陆
+  getVideoList: "/api/videoList", //获取视频列表
+  likeVideo: "/api/likeVideo",//点赞视频
 };
 
 export default Api;
