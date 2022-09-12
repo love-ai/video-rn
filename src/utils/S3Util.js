89ed1acae23abe466c7926cbfef5a84dcf82e8eb
zhuxiaowei <1649201921@qq.com>
2022-09-12 21:20:32 +0800

    【需求】修改

--- src/utils/S3Util.js
+++ src/utils/S3Util.js
@@ -1,8 +1,9 @@
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
+import { S3Client } from "@aws-sdk/client-s3";
+import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
 import { Upload } from "@aws-sdk/lib-storage";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-
 // 对S3进行配置
 const REGION = "ap-northeast-1";
 const s3Client = new S3Client({
@@ -13,38 +14,45 @@ const s3Client = new S3Client({
   }),
 });
 
+
 export default async function UploadFile(bucket, key, uri, onProgress, onSuccess) {
   try {
     const response = await fetch(uri);
     const blob = await response.blob();
-    console.log("blob:" + blob);
-
-    s3Client.send(new PutObjectCommand({
-      bucket:bucket,
-      key:key,
-      Body: blob,
-      GrantReadACP: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
-      GrantRead: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
-    }));
-
-    // const _upload = new Upload({
-    //   client: s3Client,
-    //   params: {
-    //     Bucket: bucket,
-    //     Key: key,
-    //     Body: blob,
-    //     GrantReadACP: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
-    //     GrantRead: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
-    //   },
+    // console.log("blob:" + blob);
+    // const option = {};
+    //
+    // s3Client.send(new PutObjectCommand({
+    //   Bucket: bucket,
+    //   Key: key,
+    //   Body: blob,
+    //   GrantReadACP: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
+    //   GrantRead: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
+    // }), option, (err, data) => {
+    //   if (err) {
+    //     console.log("err:" + err);
+    //   } else {
+    //     onSuccess(data.ETag);
+    //   }
     // });
-    // _upload.on("httpUploadProgress", (progress) => {
-    //   const percent = (100 * progress.loaded) / progress.total;
-    //   console.log("percent:" + percent);
-    //   onProgress(percent);
-    // });
-    // let res = await _upload.done();
-    // onSuccess(res);
-    // return res;
+    const upload = new Upload({
+      client: s3Client,
+      params: {
+        Bucket: bucket,
+        Key: key,
+        Body: blob,
+        GrantReadACP: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
+        GrantRead: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
+      },
+    });
+    upload.on("httpUploadProgress", (progress) => {
+      const percent = (100 * progress.loaded) / progress.total;
+      console.log("percent:" + percent);
+      onProgress(percent);
+    });
+    let res = await upload.done();
+    onSuccess(res);
+    return res;
   } catch (error) {
     console.log(error);
     throw new Error(error.message);
